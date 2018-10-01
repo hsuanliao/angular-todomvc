f5293dd296a6ac5303cc709082d0e41dd5936a89
Kevin Liao <kevin.liao@mail.aircast.biz>
2018-10-01 14:25:06 +0800

    5. 套用雙向繫結 => 將step 4的text value套用two-way binding, 在step 4事件中將值寫入陣列中.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   inputHint: string = 'What needs to be done?';
+  todos: string[] = [];
+  newTodoItem: string;
 
   addNewTodoItem() {
-    console.log('click add new todo item!');
+    console.log('new todo is ' + this.newTodoItem);
+
+    this.todos.push(this.newTodoItem);
+    this.newTodoItem = '';
   }
 }
