7e4eb20af26d843ef7971267fe0123e897667b58
Kevin Liao <kevin.liao@mail.aircast.biz>
2018-10-02 11:18:21 +0800

    4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   textHint = 'What needs to be done????';
+
+  addNewTodoItem() {
+    console.log('add success!');
+  }
 }
