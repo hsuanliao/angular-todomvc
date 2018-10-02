d728cf337bd3c75739b588b403fc3a9b436d731a
Kevin Liao <kevin.liao@mail.aircast.biz>
2018-10-02 11:44:40 +0800

    7-2

--- src/app/shared/todo.model.ts
+++ src/app/shared/todo.model.ts
@@ -1,10 +1,19 @@
 export class Todo {
-  public value: string;
-  public isDone: boolean;
+  // public value: string;
+  // public isDone: boolean;
   public isEdit: boolean;
 
-  constructor(value: string,
-              isDone: boolean) {
+  // constructor(value: string,
+  //             isDone: boolean) {
+  //   this.value = value;
+  //   this.isDone = isDone;
+  //   this.isEdit = false;
+  // }
+
+  constructor(
+      public value: string,
+      public isDone: boolean
+  ) {
     this.value = value;
     this.isDone = isDone;
     this.isEdit = false;
